e719ab01688f77667329d9c7e4876cbafbb6beea
PaulHard2020 <68113776+PaulHard2020@users.noreply.github.com>
2025-06-03 19:18:57 +0300

    Create ref.sh

---
+++ ref.sh
@@ -0,0 +1,8 @@
+reflector \
+  --country Finland,Germany,Netherlands,Switzerland \
+  --protocol https \
+  --age 6 \
+  --sort rate \
+  --latest 5 \
+  --verbose \
+  --save /etc/pacman.d/mirrorlist
